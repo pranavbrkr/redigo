e6545b9de8e80726491bf0734c1b3e870e57fecc
pranavbrkr <pranavbrkr@gmail.com>
2025-12-29 20:38:03 -0700

    start tcp listener and accept connections

--- cmd/redigo/main.go
+++ cmd/redigo/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Println("redigo: starting up (stub)")
+	port := flag.Int("port", 6379, "TCP port to listen on")
+	flag.Parse()
+
+	addr := fmt.Sprintf(":%d", *port)
+
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("failed to listen on %s: %v", addr, err)
+	}
+	defer ln.Close()
+
+	log.Printf("redigo listening on %s", addr)
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Printf("accept error: %v", err)
+			continue
+		}
+
+		log.Printf("accepted connection from %s", conn.RemoteAddr())
+		_ = conn.Close()
+	}
 }
