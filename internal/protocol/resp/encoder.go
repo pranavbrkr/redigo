222cf93f6ec039b05e760453c33c4a59a4654aa6
pranavbrkr <pranavbrkr@gmail.com>
2025-12-31 10:38:05 -0700

    added RESP2 encoder helpers

---
+++ internal/protocol/resp/encoder.go
@@ -0,0 +1,66 @@
+package resp
+
+import (
+	"bufio"
+	"strconv"
+)
+
+// Writes RESP simple string: +<text>\r\n
+func WriteSimpleString(w *bufio.Writer, s string) error {
+	if _, err := w.WriteString("+"); err != nil {
+		return err
+	}
+	if _, err := w.WriteString(s); err != nil {
+		return err
+	}
+	_, err := w.WriteString("\r\n")
+	return err
+}
+
+// Writes RESP error: -<text>\r\n
+func WriteError(w *bufio.Writer, msg string) error {
+	if _, err := w.WriteString("-"); err != nil {
+		return err
+	}
+	if _, err := w.WriteString(msg); err != nil {
+		return err
+	}
+	_, err := w.WriteString("\r\n")
+	return err
+}
+
+// Writes RESP Integer: <n>\r\n
+func WriteInteger(w *bufio.Writer, n int64) error {
+	if _, err := w.WriteString(":"); err != nil {
+		return err
+	}
+	if _, err := w.WriteString(strconv.FormatInt(n, 10)); err != nil {
+		return err
+	}
+	_, err := w.WriteString("\r\n")
+	return err
+}
+
+// Writes RESP Bulk String: $<len>\r\n<bytes>\r\n
+// If b is nil, writes Null Bulk String: $-1\r\n
+func WriteBulkString(w *bufio.Writer, b []byte) error {
+	if b == nil {
+		_, err := w.WriteString("$-1\r\n")
+		return err
+	}
+
+	if _, err := w.WriteString("$"); err != nil {
+		return err
+	}
+	if _, err := w.WriteString(strconv.Itoa(len(b))); err != nil {
+		return err
+	}
+	if _, err := w.WriteString("\r\n"); err != nil {
+		return err
+	}
+	if _, err := w.Write(b); err != nil {
+		return err
+	}
+	_, err := w.WriteString("\r\n")
+	return err
+}
