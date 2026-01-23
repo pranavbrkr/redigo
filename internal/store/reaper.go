3e98d8587235a25a4b5abc2b442dfde037cb300d
pranavbrkr <pranavbrkr@gmail.com>
2026-01-22 17:30:32 -0700

    make reaper stop() idempotent to prevent double-close panic

--- internal/store/reaper.go
+++ internal/store/reaper.go
@@ -1,15 +1,20 @@
 package store
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 // StartReaper starts a background goroutine that periodically deletes expired keys.
 // It returns a stop() function you MUST call to stop the goroutine.
+// stop() is idempotent (safe to call multiple times).
 func (s *Store) StartReaper(interval time.Duration) (stop func()) {
 	if interval <= 0 {
 		interval = 1 * time.Second
 	}
 
 	done := make(chan struct{})
+	var once sync.Once
 
 	go func() {
 		t := time.NewTicker(interval)
@@ -25,7 +30,11 @@ func (s *Store) StartReaper(interval time.Duration) (stop func()) {
 		}
 	}()
 
-	return func() { close(done) }
+	return func() {
+		once.Do(func() {
+			close(done)
+		})
+	}
 }
 
 func (s *Store) deleteExpired(now time.Time) {
